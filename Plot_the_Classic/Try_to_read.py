a74a3c812cba548d937042f58530d4c258c22f4b
SUKIT DEEYIENGYANG <N097484@nsk.com>
2020-07-08 00:01:26 +0700

    initial commit

---
+++ Plot_the_Classic/Try_to_read.py
@@ -0,0 +1,9 @@
+# Read two books, fast!
+
+huck_finn_url = 'https://www.inferentialthinking.com/data/huck_finn.txt'
+huck_finn_text = read_url(huck_finn_url)
+huck_finn_chapters = huck_finn_text.split('CHAPTER ')[44:]
+
+little_women_url = 'https://www.inferentialthinking.com/data/little_women.txt'
+little_women_text = read_url(little_women_url)
+little_women_chapters = little_women_text.split('CHAPTER ')[1:]
